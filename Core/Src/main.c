7aec8d658d413379a1ea5cda79258c534c2e6f0e
LPT0317 <75077760+LPT0317@users.noreply.github.com>
2022-10-03 14:11:44 +0700

    ex6

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -103,7 +103,7 @@ int main(void)
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim2);
-  setTimer0(1000);
+  setTimer0(10);
   /* USER CODE END 2 */
 
   /* Infinite loop */
